bf9ec662dde16c534bf29fd96b75aee9bd04208e
eulbyvan <work.issdq@gmail.com>
2022-11-23 15:56:38 +0700

    updated email validation

--- src/main/java/com/enigmacamp/livecode3/validation/Validation.java
+++ src/main/java/com/enigmacamp/livecode3/validation/Validation.java
@@ -1,13 +1,19 @@
 package com.enigmacamp.livecode3.validation;
 
+import org.apache.commons.validator.routines.EmailValidator;
+
 import java.util.regex.Pattern;
 
 public abstract class Validation {
+    private static final EmailValidator emailValidator = EmailValidator.getInstance();
     public static Boolean validateEmail(String email) {
-        String regexPattern = "^(.+)@(\\\\S+)$";
-
-        return Pattern.compile(regexPattern)
-                .matcher(email)
-                .matches();
+        return emailValidator.isValid(email);
     }
+
+//    It allows numeric values from 0 to 9.
+//    Both uppercase and lowercase letters from a to z are allowed.
+//    Allowed are underscore “_”, hyphen “-“, and dot “.”
+//    Dot isn't allowed at the start and end of the local part.
+//    Consecutive dots aren't allowed.
+//    For the local part, a maximum of 64 characters are allowed.
 }
